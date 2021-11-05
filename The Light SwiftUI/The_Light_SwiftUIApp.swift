532eb4f4fad5a8fa4d6a766a640791fc067db809
Anna <buanse@gmail.com>
2021-11-05 19:21:35 +0400

    Initial Commit

---
+++ The Light SwiftUI/The_Light_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Light_SwiftUIApp.swift
+//  The Light SwiftUI
+//
+//  Created by Dmitry Kost on 05.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct The_Light_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
